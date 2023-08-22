46a9552e366dc25d6c4a32db1cf8dbdec7d4d53d
Danilo Vicentin S <danilovicentindasilva@gmail.com>
2023-08-21 21:51:37 -0300

    Implementing Pages

---
+++ src/pages/Login.tsx
@@ -0,0 +1,13 @@
+import { LoginStyled } from "../components/LoginStyled/LoginStyled";
+
+const Login = () => {
+  return (
+    <LoginStyled>
+      <section>
+        <h1>Login</h1>
+      </section>
+    </LoginStyled>
+  );
+};
+
+export default Login;
