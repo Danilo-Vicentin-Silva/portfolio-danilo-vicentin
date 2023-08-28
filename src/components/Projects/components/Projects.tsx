064862c079f0a26342f4ef201359677277fd9cc6
Danilo Vicentin S <danilovicentindasilva@gmail.com>
2023-08-28 16:59:04 -0300

    Projects Translation

--- src/components/Projects/components/Projects.tsx
+++ src/components/Projects/components/Projects.tsx
@@ -1,93 +1,95 @@
-import Project from "./Project"
-import { v4 as uuid } from "uuid"
+import Project from "./Project";
+import { v4 as uuid } from "uuid";
 import {
   CordelModernoImages,
   RacingF1Images,
   SiteAndroidImages,
   TaskListImages,
   TelaLoginImages,
-} from "../../Imports/importImages"
-import { videos } from "../../Imports/ImportVideos"
-import { IoLogoJavascript } from "react-icons/io"
-import { MdOutlineHtml, MdCss } from "react-icons/md"
-import { BiLogoReact } from "react-icons/bi"
-import TopicSeparator from "../../TopicSeparator/components/TopicSeparator"
-import { ProjectsStyled } from "../style/style"
+} from "../../Imports/importImages";
+import { videos } from "../../Imports/ImportVideos";
+import { IoLogoJavascript } from "react-icons/io";
+import { MdOutlineHtml, MdCss } from "react-icons/md";
+import { BiLogoReact } from "react-icons/bi";
+import TopicSeparator from "../../TopicSeparator/components/TopicSeparator";
+import { ProjectsStyled } from "../style/style";
+import { useTranslation } from "react-i18next";
 
 const Projects = () => {
+  const { t } = useTranslation();
 
   return (
-    <ProjectsStyled >
-          <TopicSeparator title="Projetos" />
-          <h4>Destaques</h4>
+    <ProjectsStyled>
+      <TopicSeparator title={t("projectsTitle")} />
+      <h4>{t("highlights")}</h4>
 
-          <ul>
-            <Project
-              title="Racing F1"
-              date="28 de Outubro de 2022"
-              description="Meu primeiro projeto em Front End, este que foi um exercício pensado por mim mesmo afim de comprovar minhas capacidades nas principais tecnologias. Site focado na informação geral da categoria máxima do automobilismo, este que contêm um design inspirado nas cores tema da Formula 1."
-              images={RacingF1Images}
-              skillsUsed={[<IoLogoJavascript />, <MdOutlineHtml />, <MdCss />]}
-              websiteLink="https://danilo-vicentin-silva.github.io/RacingF1/"
-              githubLink="https://github.com/Danilo-Vicentin-Silva/RacingF1"
-              video={videos.RacingF1Video}
-              key={uuid()}
-            />
-            <Project
-              title="Task List"
-              date="27 de Janeiro de 2023"
-              description="Lista de tarefas online, criada para fins didáticos no estudo das hooks do React. Pode ser anotado tarefas e conclução/exclução delas. Este projeto pessoal foi criado com o objetivo de consolidar meus estudos de react, assim como esse portfólio e vários outros projetos"
-              githubLink="https://github.com/Danilo-Vicentin-Silva/Task-list"
-              websiteLink="https://danilotasklist.netlify.app/"
-              images={TaskListImages}
-              skillsUsed={[
-                <BiLogoReact />,
-                <IoLogoJavascript />,
-                <MdCss />,
-                <MdOutlineHtml />,
-              ]}
-              video={videos.TaskListVideo}
-              key={uuid()}
-            />
-            <Project
-              title="Tela de Login"
-              description="Tela de Login (sem autenticação) criada para ser uma desmostração de minhas capacidades de desenvolver um design simples de uma tela de login responsiva. Proposta didática do meu curso de HTML&CSS para fins de consolidar alguns conhecimentos sobre formulários HTML e telas de login com CSS."
-              date="26 de Outubro de 2022"
-              skillsUsed={[<MdOutlineHtml />, <MdCss />, <IoLogoJavascript />]}
-              githubLink="https://github.com/Danilo-Vicentin-Silva/Tela-Login"
-              websiteLink="https://danilo-vicentin-silva.github.io/Tela-Login/"
-              images={TelaLoginImages}
-              video={videos.TelaLoginVideo}
-              key={uuid()}
-            />
-          </ul>
-          <h4>Mais projetos</h4>
-          <ul>
-            <Project
-              title="Cordel Moderno"
-              description="Cordel Brasilleiro representado em um site que possuí um um belíssimo efeito paralax, permitindo mais estilo e temática a esse site. Outra proposta didática com o objetivo de consolidar meus conhecimentos com alguns efeitos gerados em CSS, como o efeito Paralax, um belíssimo efeito gerado com CSS."
-              date="6 de Julho de 2022"
-              skillsUsed={[<MdOutlineHtml />, <MdCss />]}
-              githubLink="https://github.com/Danilo-Vicentin-Silva/Cordel-Moderno"
-              websiteLink="https://danilo-vicentin-silva.github.io/Cordel-Moderno/"
-              images={CordelModernoImages}
-              video={videos.CordelModernoVideo}
-              key={uuid()}
-            />
-            <Project
-              title="Site Android"
-              description="Site no estilo informativo que aplica quase todos os conceitos básicos que estudei em HTML&CSS, esse site tem o objetivo de contar a histório da criação do ícone do sistema Android, uma histório bem curiosa por sinal. Essa proposta didática foi ótima para consolidar minha base em HTML&CSS."
-              date="14 de Setembro de 2022"
-              githubLink="https://github.com/Danilo-Vicentin-Silva/Site-Android"
-              websiteLink="https://danilo-vicentin-silva.github.io/Site-Android/"
-              skillsUsed={[<MdOutlineHtml />, <MdCss />]}
-              images={SiteAndroidImages}
-              video={videos.SiteAndroidVideo}
-              key={uuid()}
-            />
-          </ul>
+      <ul>
+        <Project
+          title="Racing F1"
+          date={t("racingF1Date")}
+          description={t("racingF1Description")}
+          images={RacingF1Images}
+          skillsUsed={[<IoLogoJavascript />, <MdOutlineHtml />, <MdCss />]}
+          websiteLink="https://danilo-vicentin-silva.github.io/RacingF1/"
+          githubLink="https://github.com/Danilo-Vicentin-Silva/RacingF1"
+          video={videos.RacingF1Video}
+          key={uuid()}
+        />
+        <Project
+          title="Task List"
+          date={t("taskListDate")}
+          description={t("taskListDescription")}
+          githubLink="https://github.com/Danilo-Vicentin-Silva/Task-list"
+          websiteLink="https://danilotasklist.netlify.app/"
+          images={TaskListImages}
+          skillsUsed={[
+            <BiLogoReact />,
+            <IoLogoJavascript />,
+            <MdCss />,
+            <MdOutlineHtml />,
+          ]}
+          video={videos.TaskListVideo}
+          key={uuid()}
+        />
+        <Project
+          title={t("loginScreenTitle")}
+          description={t("loginScreenDescription")}
+          date={t("loginScreenDate")}
+          skillsUsed={[<MdOutlineHtml />, <MdCss />, <IoLogoJavascript />]}
+          githubLink="https://github.com/Danilo-Vicentin-Silva/Tela-Login"
+          websiteLink="https://danilo-vicentin-silva.github.io/Tela-Login/"
+          images={TelaLoginImages}
+          video={videos.TelaLoginVideo}
+          key={uuid()}
+        />
+      </ul>
+      <h4>{t("moreProjects")}</h4>
+      <ul>
+        <Project
+          title={t("brazilianCordelTitle")}
+          description={t("brazilianCordelDescription")}
+          date={t("brazilianCordelDate")}
+          skillsUsed={[<MdOutlineHtml />, <MdCss />]}
+          githubLink="https://github.com/Danilo-Vicentin-Silva/Cordel-Moderno"
+          websiteLink="https://danilo-vicentin-silva.github.io/Cordel-Moderno/"
+          images={CordelModernoImages}
+          video={videos.CordelModernoVideo}
+          key={uuid()}
+        />
+        <Project
+          title={t("websiteAndroidTitle")}
+          description={t("websiteAndroidDescription")}
+          date={t("websiteAndroidDate")}
+          githubLink="https://github.com/Danilo-Vicentin-Silva/Site-Android"
+          websiteLink="https://danilo-vicentin-silva.github.io/Site-Android/"
+          skillsUsed={[<MdOutlineHtml />, <MdCss />]}
+          images={SiteAndroidImages}
+          video={videos.SiteAndroidVideo}
+          key={uuid()}
+        />
+      </ul>
     </ProjectsStyled>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
