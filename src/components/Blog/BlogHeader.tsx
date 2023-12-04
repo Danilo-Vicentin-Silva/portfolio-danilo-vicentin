73aa8e8e7920b82a672362f26189676a94563fb6
Danilo Vicentin S <Danilovicentindasilva@gmail.com>
2023-12-04 11:36:50 -0300

    Apresantation Content

--- src/components/Blog/BlogHeader.tsx
+++ src/components/Blog/BlogHeader.tsx
@@ -15,9 +15,11 @@ const BlogHeader = () => {
         </small>
       </p>
       <p>
-        Desenvolvedor de Sites focado em criar as melhores soluções web. Foco,
-        Trabalho & Disciplina são os meus objetivos enquanto trabalho, estudo e
-        crio.
+        Desenvolvedor de Sites focado em criar as melhores soluções web.
+        Brasileiro, nascido em Campinas-SP e focando sempre em aprender mais e mais dentro da minha área,
+        onde todos os dias surge uma tecnologia nova. Sempre busco melhorar e
+        dar meu máximo para qualquer que seja o projeto/trabalho. Foco, Trabalho
+        & Disciplina são os meus objetivos enquanto trabalho, estudo e crio.
       </p>
     </BlogHeaderStyled>
   )
