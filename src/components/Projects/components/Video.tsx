8960b0c61cd121c77d4766eb5b664929aebfa4e9
Danilo Vicentin S <Danilovicentindasilva@gmail.com>
2023-12-06 09:31:18 -0300

    Videos Fix

--- src/components/Projects/components/Video.tsx
+++ src/components/Projects/components/Video.tsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React from "react"
 interface Props {
-  video: string;
+  video: string
 }
 
 const Video: React.FC<Props> = ({ video }) => {
   return (
-    <video controls autoPlay loop>
-      <source src={video} type="video/mp4" />
-      Seu navegador nao suporta esse recurso
-    </video>
-  );
-};
+    <iframe
+      className="video"
+      src={video}
+      title="YouTube video player"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowFullScreen
+    >
+      Seu navegador não suporta esse recurso
+    </iframe>
+  )
+}
 
-export default Video;
+export default Video
