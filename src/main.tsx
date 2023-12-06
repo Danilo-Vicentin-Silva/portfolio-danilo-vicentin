17c801f289e01f02dfe183742c60cd0ad0625bce
Danilo Vicentin S <Danilovicentindasilva@gmail.com>
2023-12-06 10:35:28 -0300

    Try Routes Fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,22 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import Home from "./pages/Home.tsx"
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import "./services/i18n/index"
 import ErrorPage from "./pages/ErrorPage.tsx"
 import ScrollTop from "./components/BackOnTop/components/BackOnTop.tsx"
 import { Analytics } from "@vercel/analytics/react"
 import Blog from "./pages/Blog.tsx"
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/blog",
-    element: <Blog />,
-    errorElement: <ErrorPage />,
-  },
-])
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ScrollTop />
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} errorElement={<ErrorPage />} />
+        <Route path="/blog" element={<Blog />} errorElement={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
     <Analytics />
   </React.StrictMode>
 )
